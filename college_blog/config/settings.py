245c9e265f09ff9aae0d2e75f2e972b228288cf2
RajanyakDas <rajo.zxy42@gmail.com>
2026-04-09 15:50:34 +0530

    Initial Commit: Restarting project with clean architecture

--- college_blog/config/settings.py
+++ college_blog/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b05m3pbtx-^4)gr6g4c-ui6fl5ao6pja3zb-uvi&(+#h#_ox0j'
+SECRET_KEY = 'django-insecure-4@db_g%*h81856ng3ir=4q^qoxk4jip1@qmh)ht843fmg#sfu_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
 ]
 
 MIDDLEWARE = [
