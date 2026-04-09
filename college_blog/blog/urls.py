66afa6e70fbbcbf4c6277f59f5a08005d772660e
RajanyakDas <rajo.zxy42@gmail.com>
2026-04-09 17:27:58 +0530

    Chapter 2: Added EmailPostForm and post_share view

--- college_blog/blog/urls.py
+++ college_blog/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     #path('', views.post_list, name='post_list'), we will replace this with the class-based view below
     path('', views.PostListView.as_view(), name='post_list'), # New Class View
     path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
