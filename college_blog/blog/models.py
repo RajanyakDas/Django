b681799bd6816ca666e2bf27f69263df2db6d13a
RajanyakDas <rajo.zxy42@gmail.com>
2026-04-09 15:58:11 +0530

    Initial Post model with title, slug, author, and body

--- college_blog/blog/models.py
+++ college_blog/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
